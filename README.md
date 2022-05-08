8ec15c08b5d4769fdc02e111ef4154e0cac1e86b
EunjinJwa <ejjwa0563@gmail.com>
2022-05-09 00:20:23 +0900

    node module http

---
+++ README.md
@@ -0,0 +1,22 @@
+Node.js Tutorial 
+
+참조 : https://www.w3schools.com/nodejs/nodejs_get_started.asp
+
+## Module
+모듈은 javascript의 라이브러리와 같다.
+모둘을 추가하기 위해서는 `require()` function을 모듈이름과 함께 사용하면 된다.
+```
+var http = require('http')
+```
+node의 내장 모듈 참조 : [[Built-in Modules]](https://www.w3schools.com/nodejs/ref_modules.asp)
+
+### 나의 모듈 생성
+* `export` 키워드로 나의 모듈을 작성할 수 있다.
+* myModule.js 참조 
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
+const dt = require('./myModule.js');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello My Node WebServer !! ');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  res.writeHead(200, {'content-Type': 'text/html'})
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write(req.url);
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(port);
